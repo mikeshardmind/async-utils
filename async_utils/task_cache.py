1cbc865127af3c9474e3f1ac7d7a63befd07c49c
Michael H <michael@michaelhall.tech>
2024-10-08 17:42:17 -0400

    Add task clearing methods

--- async_utils/task_cache.py
+++ async_utils/task_cache.py
@@ -18,14 +18,18 @@ import asyncio
 from collections.abc import Callable, Coroutine, Hashable
 from functools import partial
 from typing import Any, ParamSpec, TypeVar
+from weakref import WeakKeyDictionary
 
 from ._cpython_stuff import make_key
 
-__all__ = ("taskcache",)
+__all__ = ("taskcache", "clear_cache", "remove_cache_entry")
 
 
 P = ParamSpec("P")
 T = TypeVar("T")
+
+
+_caches: WeakKeyDictionary[Hashable, dict[Hashable, asyncio.Task[Any]]] = WeakKeyDictionary()
 
 
 def taskcache(
@@ -40,6 +44,8 @@ def taskcache(
     Note: This uses the args and kwargs of the original coroutine function as a cache key.
     This includes instances (self) when wrapping methods.
     Consider not wrapping instance methods, but what those methods call when feasible in cases where this may matter.
+
+    The ordering of args and kwargs matters.
     """
 
     def wrapper(coro: Callable[P, Coroutine[Any, Any, T]]) -> Callable[P, asyncio.Task[T]]:
@@ -63,6 +69,33 @@ def taskcache(
                     task.add_done_callback(call_after_ttl)
                 return task
 
+        _caches[wrapped] = internal_cache
         return wrapped
 
     return wrapper
+
+
+def clear_cache(f: Callable[..., Any]) -> None:
+    """
+    Clear the cache of a decorated function
+    """
+    cache = _caches.get(f)
+    if cache is None:
+        raise RuntimeError(f"{f:!r} is not a function wrapped with taskcache")
+    cache.clear()
+
+
+def remove_cache_entry(f: Callable[..., Any], *args: Hashable, **kwargs: Hashable):
+    """
+    Remove the cache entry for a specific arg/kwarg combination.
+
+    The ordering of args and kwargs must match.
+
+    Will not error under a missing key under the assumption that a race condition on removal
+    for various reasons (such as ttl) could occur
+    """
+
+    cache = _caches.get(f)
+    if cache is None:
+        raise RuntimeError(f"{f:!r} is not a function wrapped with taskcache")
+    cache.pop(make_key(args, kwargs))
