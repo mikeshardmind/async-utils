83006c8de00240a1a2c4b13b97778c3de4555b10
Michael Hall <michael@michaelhall.tech>
2026-04-10 09:49:51 -0400

    ruff ignores in misc/

--- _misc/_ensure_breaking_of_merged_gens_doesnt_overconsume.py
+++ _misc/_ensure_breaking_of_merged_gens_doesnt_overconsume.py
@@ -1,9 +1,9 @@
-from async_utils._merge_gens import batch_merge_gens
+from async_utils._merge_gens import batch_merge_gens  # noqa: PLC2701
 
 
 async def _g(n: int):
     for val in range(n, n + 100):
-        print("Produced", val, flush=True)
+        print("Produced", val, flush=True)  # noqa: T201
         yield val
 
 
@@ -11,7 +11,7 @@ async def main():
     async for batch in batch_merge_gens(_g(100), _g(200), _g(300)):
         should_break = False
         for val in batch:
-            print("Consumed", val, flush=True)
+            print("Consumed", val, flush=True)  # noqa: T201
             if val == 250:
                 should_break = True
 
